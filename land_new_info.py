20ddc4329dd55815c2c4fefc95585ed5bf77674b
ymjung <metalbird0@gmail.com>
2017-01-21 22:54:10 +0900

    integration land_new py

--- land_new_info.py
+++ land_new_info.py
@@ -4,6 +4,8 @@ from telegram.ext import Updater
 import configparser
 import random
 import sys
+import datetime
+import time
 
 cf = configparser.ConfigParser()
 cf.read('config.cfg')
@@ -11,18 +13,14 @@ cf.read('config.cfg')
 VALID_USER=cf.get('telegram','VALID_USER')
 TOKEN=cf.get('telegram','TOKEN')
 
-sindongaUrl = cf.get('land_url','sindongaUrl')
-ggachiUrl = cf.get('land_url', 'ggachiUrl')
-daechiUrl = cf.get('land_url', 'daechiUrl')
-suseoUrl = cf.get('land_url', 'suseoUrl')
+landUrls = cf.get('land_url','URLS')
+landUrls = landUrls.split(',')
 
 LIMIT_PRICE = 20000
 SLEEP_SEC = random.randint(120,300)
-BREAK_LIMIT = 5
-BREAK_TIME = 0
 
-urls = {'SIN-DONG-A': sindongaUrl, 'GGA-CHI': ggachiUrl, 'DAE-CHI':daechiUrl, 'SU-SEO':suseoUrl}
-
+RETRY_LIMIT_CNT=5
+RETRY_LIMIT={datetime.date.today(): RETRY_LIMIT_CNT}
 
 
 
@@ -30,6 +28,7 @@ def get_sale_products(findUrl) :
 	soup = BeautifulSoup(requests.get(findUrl).text, "html.parser")
 	table = soup.find("table", { "class" : "sale_list _tb_site_img NE=a:cpm"})
 	trs = table.find("tbody").find_all('tr')
+	name = soup.find(id='complexListLayer').find('a', {'class':'on'}).text
 	results = list()
 
 	for tr in trs:
@@ -39,7 +38,7 @@ def get_sale_products(findUrl) :
 			floor = tr.find_all('td', {'class':"num2"})[1].text
 			budongsan = tr.find('td', {'class':'contact'}).find_all('span')[0]['title']
 			contact = tr.find('td', {'class':'contact'}).find_all('span')[1].text
-			crol = {'price':price,'dong':dong,'floor':floor,'budongsan':budongsan,'contact':contact}
+			crol = {'name':name,'price':price,'dong':dong,'floor':floor,'budongsan':budongsan,'contact':contact}
 			results.append(crol)
 		except AttributeError:
 			continue
@@ -47,38 +46,42 @@ def get_sale_products(findUrl) :
 
 def get_line_up(products):
 	result = ''
-	for key in products:
-		result += '[PRODUCT]:: ' + key + '\n'
-		for product in products[key]:
-			result += '\t'+str(product) + '\n'
+	for product in products:
+		result += '[' + product['name'] + '] \t' + str(product) + '\n'
 	return result
 
 def get_new():
-	products = {}
-	for key in urls.keys():
-		products[key] = get_sale_products(urls[key])[0:3]
+	products=[] 
+	for landUrl in landUrls: 
+		getProducts = get_sale_products(landUrl)[0:3]
+		products.extend(getProducts)
 	return products
 
-
-
-import time
+def is_break():
+	retryCnt = get_date_retry_limit(datetime.date.today())
+	if retryCnt<0:
+		return True
+	return False
+def get_date_retry_limit(date):
+	dateStr = str(date)
+	if dateStr in RETRY_LIMIT:
+		print('reduce today limit ', dateStr, RETRY_LIMIT[dateStr])
+		RETRY_LIMIT[dateStr] -= 1
+	else:
+		print('make today limit ', dateStr)
+		RETRY_LIMIT.update({dateStr: RETRY_LIMIT_CNT})
+	return RETRY_LIMIT[dateStr]
 
 check_flag = False
 seen_set = set()
-
 while True:
 	try :
 		products = get_new()	
-		for key in products:
-			product = products[key]
-			for each in product:
-				check_key = key + each['price'] + each['contact'] + each['floor']
-				if (check_key not in seen_set) and (int(each['price'].replace(',','')) <= LIMIT_PRICE): 
-					check_flag = True
-					seen_set.add(check_key)
-			if check_flag is True:
-				print('break')
-				break
+		for product in products:
+			check_key = product['name'] + product['price'] + product['contact'] + product['floor']
+			if (check_key not in seen_set) and (int(product['price'].replace(',','')) <= LIMIT_PRICE): 
+				check_flag = True
+				seen_set.add(check_key)
 		if check_flag is True:
 			msg = get_line_up(products)
 			print(msg)
@@ -87,11 +90,12 @@ while True:
 			check_flag = False
 		else :
 			print('none')
-		time.sleep(SLEEP_SEC)
+			time.sleep(SLEEP_SEC)
 	except :
-		print('unexpect error.', sys.exc_info())
-		if BREAK_LIMIT < BREAK_TIME :
+		print('unexpected error', sys.exc_info()[0])
+		print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+		if is_break():
 			break
-		else :
-			BREAK_TIME += 1
-			continue
+		continue
+
+
