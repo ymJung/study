5afcf9695398748522d9bf6f8deaaa90f6d8d333
ymJung <metalbird0@gmail.com>
2019-09-07 08:49:25 +0900

    integration bulpan scrap

--- pgr_comment_scrap.py
+++ pgr_comment_scrap.py
@@ -7,24 +7,41 @@ import urllib.parse
 from urllib import parse
 
 
-search_str = '2019 우리은행 챔피언스 코리아 서머 플레이오프 2R - DAMWON vs. SKT'
+search_str = '조국 청문회' #검색할 불판명 쓸것 (숫자제외)
 parse_str = parse.quote(search_str)
-search_url = 'https://pgr21.com/pb/pb.php?id=bulpan&ss=on&sc=on&keyword=' + parse_str
-pan_url = 'https://pgr21.com/pb/pb.php?id=bulpan&no='
+search_url = 'https://pgr21.com/pb/pb.php?id=bulpan&page=1&ss=on&sc=on&keyword=' + parse_str
 
+pan_url = 'https://pgr21.com/pb/pb.php?id=bulpan&no='
+default_page = 'https://pgr21.com/'
 def get_soup(url) :        
     with urllib.request.urlopen(url) as response:
+        print('crawlling.. ' + url)
         time.sleep(random.randrange(0, 5)) # 너무 자주 긁어서 디도스로 오인받아 차단당할라..
         html = response.read()
         return BeautifulSoup(html, 'html.parser')
 
 soup = get_soup(search_url)
-a_tags = soup.find_all('a')
+pages = soup.find(class_='pagination')
+
+DEFAULT_PAGE_A_TAG_CNT = 3
+ONE_PAGE = 1
+a_tags = []
+
+if len(pages) > DEFAULT_PAGE_A_TAG_CNT:
+    for page in pages:
+        if 'page=' in page['href'] :
+            page_per_pan_soup = get_soup(default_page + page['href'])
+            p_p_tags = page_per_pan_soup.find_all('a')
+            a_tags.extend(p_p_tags)
+first_page_tags = soup.find_all('a')
+
+a_tags.extend(first_page_tags)
 
 pan_urls = []
 for at in a_tags:
     if search_str in  at.text :
-        pan_no = at['href'].split('no=')[1].split('&')[0]
+        href_url = at['href']
+        pan_no = href_url[href_url.rfind('/') + 1: href_url.find('?')]        
         pan_urls.append(pan_url + pan_no)
 
 f = open(search_str, 'w', encoding='utf8')
