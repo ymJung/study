1831d74a3ba08d19ba1e874b6593431a43c94d21
ymJung <metalbird0@gmail.com>
2019-09-25 17:09:27 +0900

    bug fix

---
+++ insert_qry_from_xlsx.py
@@ -0,0 +1,79 @@
+import xlrd
+
+HEADER_ROW = 0
+file_name = '자동생성 검수 매칭 요청 건.xlsx'
+workbook = xlrd.open_workbook(file_name)
+sheet = workbook.sheet_by_index(0)
+
+
+SKIP_VALUE = ['SYSDATE']
+SKIP_COL_NUM = []
+
+def is_number(input_value):
+    try :
+        float(input_value)
+        return True
+    except ValueError:
+        return False
+
+def start_with_select(input_value):
+    if is_number(input_value) is False:
+        return input_value.find('(SELECT') >= 0
+    return False
+
+
+def attatch_text(xls_input, col_num):
+    result = ''
+    if col_num in SKIP_COL_NUM:
+        result = str(xls_input)
+    else:
+        if xls_input in SKIP_VALUE:            
+            result = str(xls_input)
+        elif is_number(xls_input):
+            try :
+                result = str(round(xls_input))   
+            except:
+                result = str(xls_input)  
+            
+        elif start_with_select(xls_input):
+            result = str(xls_input)        
+        else :
+            result = "'" + xls_input + "'"
+    return result + ","
+    
+
+###
+#INSERT INTO PD_PTNR_ATTR_MAPP
+		#(PTNR_ATTR_MAPP_SEQ, PTNR_MNFCO_NM, PTNR_BRAND_NM, CTLG_ATTR_NO1, CTLG_ATTR_VAL_NO1, CTLG_ATTR_NO2, CTLG_ATTR_VAL_NO2, CTLG_ATTR_NO3, CTLG_ATTR_VAL_NO3, ATTR_VAL_DTL_NM, CREATE_DT, CREATE_NO, UPDATE_DT, UPDATE_NO)
+		#VALUES
+        # (0, '', '', 0, 0, 0, 0, 0, 0, '', '', 0, '', 0);
+	
+###
+
+HEADER_COL_LEN = 0
+
+for cell in sheet.row(HEADER_ROW):
+    if cell.value is not '':
+        HEADER_COL_LEN += 1
+
+header_qry = 'INSERT INTO ' + sheet.name + '('
+for col in range(HEADER_COL_LEN):
+    header_qry += sheet.cell_value(HEADER_ROW, col) + ', '
+# headers 
+header_qry = header_qry[0:header_qry.rfind(',')] + ') VALUES '
+
+insert_qry = ''
+for row in range((HEADER_ROW + 1), sheet.nrows):
+    for col in range(HEADER_COL_LEN):
+        if col is 0:
+            insert_qry += header_qry + '('
+        insert_qry += attatch_text(sheet.cell_value(row, col), col)
+        if col is HEADER_COL_LEN - 1:
+            insert_qry = insert_qry[0:insert_qry.rfind(',')]
+            insert_qry += ');\n'
+
+
+f = open(file_name + '.sql', 'w')
+f.write(insert_qry)
+
+
